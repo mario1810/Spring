1307884bbc2e7ffb59f120ec4d469b62fcad7578
Mario Alvarez <mario.alvarez1810@gmail.com>
2025-05-17 14:13:43 -0600

    Add new endpoints

--- 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -11,4 +11,17 @@ public class FunRestController {
     public String sayHello(){
         return "Hello World!";
     }
+
+    //Expose new endpoint for workout
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day!";
+    }
+
+
 }
