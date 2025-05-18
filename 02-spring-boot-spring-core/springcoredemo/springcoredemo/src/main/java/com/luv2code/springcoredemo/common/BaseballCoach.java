978e292f1868c96be5ae54a8f71c74a31109310c
Mario Alvarez <mario.alvarez1810@gmail.com>
2025-05-18 13:09:09 -0600

    add lazy initialization

--- 02-spring-boot-spring-core/springcoredemo/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
+++ 02-spring-boot-spring-core/springcoredemo/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Component;
 @Primary
 public class BaseballCoach implements Coach{
 
+    public BaseballCoach(){
+        System.out.println("In constructor "+ getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Spend 20 minutes in batting practice!";
--- 02-spring-boot-spring-core/springcoredemo/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
+++ 02-spring-boot-spring-core/springcoredemo/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
@@ -1,9 +1,15 @@
 package com.luv2code.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy //Specify one class
 public class TrackCoach implements Coach{
+    public TrackCoach() {
+        System.out.println("In constructor "+ getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Run hard 5k!";
