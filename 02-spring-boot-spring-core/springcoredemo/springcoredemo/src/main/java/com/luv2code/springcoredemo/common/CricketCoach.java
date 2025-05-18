07d6bcac4a57d62ccb115f38546689e067ff3509
Mario Alvarez <mario.alvarez1810@gmail.com>
2025-05-18 13:25:37 -0600

    bean life cycle

--- 02-spring-boot-spring-core/springcoredemo/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/springcoredemo/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -1,15 +1,28 @@
 package com.luv2code.springcoredemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class CricketCoach  implements Coach{
 
     public CricketCoach() {
         System.out.println("In constructor "+ getClass().getSimpleName());
+    }
+
+    @PostConstruct
+    public void doMyStartupStuff(){
+        //During bean initialization and before dependency injection
+        System.out.println("Post construct "+ getClass().getSimpleName());
+    }
+
+    @PreDestroy // Doesnt work for prototype scope
+    public void doMyCleanUpStuff(){
+        System.out.println("Pre destroy "+ getClass().getSimpleName());
     }
 
     @Override
